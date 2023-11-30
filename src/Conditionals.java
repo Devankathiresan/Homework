b27e061fe02a94e5016d212a9156ce69927b7b04
devankathiresan <devan_kathiresan@milton.edu>
2023-11-30 10:34:19 -0500

    Loops HW10

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -7,7 +7,7 @@ public class Conditionals {
     public Conditionals(){
         String question="Do you like school?";
         System.out.println(question);
-        int randomInt = (int)(Math.random() * 101);
+        double randomInt = Math.random();
 
         if (randomInt<0.16){
 
